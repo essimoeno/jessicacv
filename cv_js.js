21c7d086146c9e1984239c40e7a97a46ffd07a36
Jess <97735925+essimoeno@users.noreply.github.com>
2023-08-25 11:50:11 +0200

    Add files via upload

---
+++ cv_js.js
@@ -0,0 +1,91 @@
+/**
+ * Resume/CV template created with Bootstrap 5 by @vmoratog and @jdnichollsc
+ */
+const resume = {
+    firstName: 'Jessica',
+    lastName: 'Moreno',
+    jobTitle: 'Software Developer',
+    city: 'Madrid',
+    postalCode: '',
+    country: 'España',
+    phone: '+34 603842567',
+    email: 'jessicamorenomar@gmail.com',
+    education: [
+      {
+        school: 'IES La arboleda',
+        degree: 'CFGS Desarrollo de Aplicaciones Web',
+        graduationDate: 'SEP 2021 - JUN 2023',
+        description: 'Como estudiante de este grado superior poseo conocimientos de programación en Java, diseño e implantación de bases de datos (SQL) y diseño e implantación de páginas web (HTML, CSS, XML, JSON y JavaScript.'
+      },
+      {
+        school: 'IES La arboleda',
+        degree: 'CFGM Imagen y Sonido',
+        graduationDate: 'SEP 2018 - JUN 2020',
+        description: 'Realización de proyectos con Adobe Premiere Pro, Adobe Photoshop , ProTools'
+      }
+    ],
+    links: [
+      {
+        label: 'GitHub',
+        link: 'https://github.com/jdnichollsc'
+      },
+      {
+        label: 'LinkedIn',
+        link: 'https://linkedin.com/in/jdnichollsc'
+      }
+    ],
+    skills: [
+      'Java',
+      'SQL',
+      'JavaScript',
+      'HTML',
+      'CSS',
+      'MVC',
+      'PHP',
+      'jQuery',
+      'Bootstrap',
+      'Hibernate',
+      'Desarrollo Salesforce',
+      'MariaDB'
+    ],
+    languages: ['English', 'Spanish'],
+    professionalSummary: `Soy una desarrolladora de software recién graduada con muchas ganas de aprender y mejorar mis habilidades , a demás de explorar nuevos métodos de trabajo y tecnologías. Estoy emocionada de embarcarme en este gratificante viaje de crecimiento profesional y descubrir las infinitas posibilidades que este sector tiene para ofrecerme.`,
+    employmentHistory: [
+      {
+        jobTitle: 'Desarrolladora de software',
+        startDate: 'Mar 2023',
+        endDate: 'Jun 2023',
+        employer: 'Accenture',
+        city: 'Remote',
+        achievements: [
+          'Desarrollo con Apex de Salesforce, Test de código, debug y resolución de problemas.',
+          'Trabajo en proyecto real realizando diferentes funciones de administración y desarrollo en la plataforma, utilizando la Developer Console del CRM y Visual Studio Code.'
+        ]
+      }
+    ],
+    photo: 'https://s3.eu-west-1.amazonaws.com/resumedone-eu-west-1/AxajI58Ph-photo.png',
+  }
+  
+  const formatResume = (r) => ({
+    ...r,
+    address: [
+      r.country,
+      r.city,
+      r.postalCode
+    ].filter(Boolean).join(', ')
+  })
+  
+  new Vue({
+    el: "#app",
+    data: formatResume(resume)
+  });
+  
+  /**
+   * Wait for animatable-component to be loaded (Only for VanillaJS)
+   **/
+  function animatableLoaded() {
+    document.querySelector('body').classList.remove('d-none');
+  }
+  if (customElements) {
+    customElements.whenDefined('animatable-component').then(animatableLoaded);
+  } else animatableLoaded()
