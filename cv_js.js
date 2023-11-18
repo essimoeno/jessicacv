66fb1674e096680e3527b4553629bc490aa6200c
Jess <97735925+essimoeno@users.noreply.github.com>
2023-11-18 10:21:12 +0100

    Add files via upload

--- cv_js.js
+++ cv_js.js
@@ -14,13 +14,13 @@ const resume = {
       {
         school: 'IES La arboleda',
         degree: 'CFGS Desarrollo de Aplicaciones Web',
-        graduationDate: 'SEP 2021 - JUN 2023',
+        graduationDate: 'SEP 2021 - JUN 2023 | IES La Arboleda',
         description: 'Como estudiante de este grado superior poseo conocimientos de programación en Java, diseño e implantación de bases de datos (SQL) y diseño e implantación de páginas web (HTML, CSS, XML, JSON y JavaScript.'
       },
       {
         school: 'IES La arboleda',
         degree: 'CFGM Imagen y Sonido',
-        graduationDate: 'SEP 2018 - JUN 2020',
+        graduationDate: 'SEP 2018 - JUN 2020 | IES La Arboleda',
         description: 'Realización de proyectos con Adobe Premiere Pro, Adobe Photoshop , ProTools'
       }
     ],
@@ -45,10 +45,13 @@ const resume = {
       'jQuery',
       'Bootstrap',
       'Hibernate',
-      'Desarrollo Salesforce',
-      'MariaDB'
+      'Desarrollo CRM Salesforce',
+      'MariaDB',
+      'Oracle SQL',
+      'Ajax',
+      'IBM Maximo for Aviation',
     ],
-    languages: ['English', 'Spanish'],
+    languages: ['Español - Nativo', 'Inglés - Nivel Medio'],
     professionalSummary: `Soy una desarrolladora de software recién graduada con muchas ganas de aprender y mejorar mis habilidades , a demás de explorar nuevos métodos de trabajo y tecnologías. Estoy emocionada de embarcarme en este gratificante viaje de crecimiento profesional y descubrir las infinitas posibilidades que este sector tiene para ofrecerme.`,
     employmentHistory: [
       {
@@ -60,6 +63,17 @@ const resume = {
         achievements: [
           'Desarrollo con Apex de Salesforce, Test de código, debug y resolución de problemas.',
           'Trabajo en proyecto real realizando diferentes funciones de administración y desarrollo en la plataforma, utilizando la Developer Console del CRM y Visual Studio Code.'
+        ]
+      },
+      {
+        jobTitle: 'Asset Management Performance',
+        startDate: 'Oct 2023',
+        endDate: 'Trabajando actualmente',
+        employer: 'Accenture',
+        city: 'Madrid',
+        achievements: [
+          'Gestión de activos, trabajando mediante IBM MAXIMO, consulta, actualización y eliminado mediante Oracle SQL. Realización de scripts con Python y JavaScript',
+          'Trabajo en equipo, soporte a cliente internacional.'
         ]
       }
     ],
